18314d4c064d3002d3bdee3e9d8cd40fdebe408d
Gabriel Atencio <gabrielatencio688@gmail.com>
2024-07-31 20:52:53 -0400

    creando el js y funciones del encriptador, siguiend los pasos de trello

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+const textArea = document.querySelector("text-area");
+const segunda__Seccion = document.querySelector(".segunda__seccion");
+
+/*
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+*/
+
+let llavesEncriptacion = [["e", "enter"], ["i", "imes"],["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+console.log(llavesEncriptacion)
